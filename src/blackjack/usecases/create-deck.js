226f80d9e41dee7c89ad01cdc5e4277787ebd196
jantonio2 <jesusrochavedia@gmail.com>
2023-02-13 21:45:02 -0400

    Refactor the project

---
+++ src/blackjack/usecases/create-deck.js
@@ -0,0 +1,22 @@
+import _ from 'underscore'
+
+// This function creates a new deck
+export const createDeck = (types, specials) => {
+
+  deck = []
+
+  for ( let i = 2; i <= 10; i++ ){
+    for ( let type of types ) {
+      deck.push( i + type )
+    }
+  }
+
+  for (let type of types) {
+    for (let esp of specials) {
+      deck.push( esp + type )
+    }
+  }
+
+  return _.shuffle( deck )
+
+}
